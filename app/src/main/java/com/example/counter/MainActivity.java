6e1044f956505fbb0805212ec017c20d81855dc3
Arslan Mushtaq <m.arslanmushtaqahmed@gmail.com>
2020-02-08 18:07:00 +0500

    completing logical structure

--- app/src/main/java/com/example/counter/MainActivity.java
+++ app/src/main/java/com/example/counter/MainActivity.java
@@ -1,14 +1,53 @@
 package com.example.counter;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView counter;
+
+    // Setting counter initial value to 0
+    private int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Finding the counter value view
+        counter = findViewById(R.id.counting_value);
+
+        // Setting value of counter to counter view
+        updateUI();
+    }
+
+    // Increase counter value
+    public void IncreaseValue(View view) {
+        // Increasing Value
+        count++;
+        updateUI();
+    }
+
+    // If value greater than 0, decrease counter value
+    public void DecreaseValue(View view) {
+        // Checking if value is greater than 0
+        if (count > 0) {
+            count--;
+            updateUI();
+        } else {
+            Toast.makeText(this, R.string.below_zero_err, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    // Helper function for setting text to counter view
+    private void updateUI() {
+        counter.setText(String.format(Locale.getDefault(), "%d", count));
     }
 }
